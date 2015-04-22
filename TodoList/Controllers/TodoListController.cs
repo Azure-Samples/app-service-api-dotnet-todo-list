7468cd894fe90b1f26f936cde1a20f297b7e1e76
bradygaster <brady.gaster@gmail.com>
2015-04-22 15:48:59 -0700

    initial create

---
+++ TodoList/Controllers/TodoListController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TodoList.App_Start;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+    public class TodoListController : ApiController
+    {
+        const string FILENAME = "todos.json";
+        private GenericStorage _storage;
+
+        private static string GetRandomFileName()
+        {
+            return Path.GetFileNameWithoutExtension(Path.GetRandomFileName());
+        }
+
+        private async Task<IEnumerable<TodoListItem>> GetTodos()
+        {
+            var todos = await _storage.Get(FILENAME);
+
+            if (todos == null)
+            {
+                await _storage.Save(new TodoListItem[]
+                    {
+                        new TodoListItem { Content = "Pick up the groceries", Id = GetRandomFileName() },
+                        new TodoListItem { Content = "Drop off the dry cleaning", Id = GetRandomFileName() },
+                    }, FILENAME);
+            }
+
+            return todos;
+        }
+
+        public TodoListController()
+        {
+            _storage = new GenericStorage();
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(IEnumerable<TodoListItem>))]
+        [Route("todos")]
+        public async Task<IEnumerable<TodoListItem>> Get()
+        {
+            return await GetTodos();
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(TodoListItem))]
+        [Route("todos/{id}")]
+        public async Task<TodoListItem> GetById([FromUri] string id)
+        {
+            var todos = await GetTodos();
+            return todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(TodoListItem))]
+        [Route("todos")]
+        public async Task<TodoListItem> Post([FromBody] TodoListItem todoItem)
+        {
+            var todos = await GetTodos();
+            var list = todos.ToList();
+            todoItem.Id = GetRandomFileName();
+            list.Add(todoItem);
+            await _storage.Save(list, FILENAME);
+            return todoItem;
+        }
+
+        [HttpDelete]
+        [ResponseType(typeof(bool))]
+        [Route("todos/{id}")]
+        public async Task<bool> Delete([FromUri] string id)
+        {
+            var todos = await GetTodos();
+            var list = todos.ToList();
+            list.RemoveAll(x => x.Id == id);
+            await _storage.Save(list, FILENAME);
+            return true;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Todo List Azure API App Sample #
+
+This is a simple example of how to build an [API App](http://azure.microsoft.com/en-us/documentation/articles/app-service-api-apps-why-best-platform/ "What are API Apps?") for deployment into the Azure App Service. This example API App uses a Web API back end and a simple HTML/AngularJS client to communicate with the API. 
+
+## More Information ##
+You can learn more about API Apps on the [Azure.com](http://azure.com "The Microsoft Azure Home Page") home page. This Todo List sample is similar to the [Contact List](https://github.com/Azure-Samples/API-Apps-DotNet-ContactList-Sample) API App Sample used throughout some of the articles outlining the process of creating, deploying, consuming, and debugging API Apps. Whereas the Contact List example goes further by providing a separate Web App that serves as a client to the API App back-end, this Todo List sample is self-contained, without a separate client Web App.  
+
+Helpful Links:
+- [API App](http://azure.microsoft.com/en-us/documentation/articles/app-service-api-apps-why-best-platform/ "What are API Apps?")
+- [Create an API App in Azure App Service](http://azure.microsoft.com/en-us/documentation/articles/app-service-dotnet-create-api-app/)
+- [Convert an Existing API to an API App](http://azure.microsoft.com/en-us/documentation/articles/app-service-dotnet-create-api-app-visual-studio/)
+- [Remotely Debug an API App](http://azure.microsoft.com/en-us/documentation/articles/app-service-dotnet-remotely-debug-api-app/)
+- [Protect an API App](http://azure.microsoft.com/en-us/documentation/articles/app-service-api-dotnet-add-authentication/)
+- [Deploy an API App in App Service](http://azure.microsoft.com/en-us/documentation/articles/app-service-dotnet-deploy-api-app/)
